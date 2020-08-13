31b2cb085e0ed574c95946135b5dd4b118c64d8e
kwhsiung <kaiwenhsiung@gmail.com>
2020-08-13 15:36:37 +0800

    feat(photographer-panhsiaohsia): add Header fade effect

--- photographer-panhsiaohsia/src/components/Header.svelte
+++ photographer-panhsiaohsia/src/components/Header.svelte
@@ -1,28 +1,44 @@
-  <header class="header">
-    <div class="header__logo-share">
-      <MirrorMediaLogo class="mirror-media-logo" />
-      <ShareButton class="share-button" />
-    </div>
-  </header>
+{#if showHeader}
+<header transition:fade="{{duration: 100}}" class="header">
+  <div class="header__logo-share">
+    <MirrorMediaLogo class="mirror-media-logo" />
+    <ShareButton class="share-button" />
+  </div>
+</header>
+{/if}
 
-  <script>
-    import MirrorMediaLogo from './components/MirrorMediaLogo.svelte'
-    import ShareButton from './components/ShareButton.svelte'
-  </script>
+<svelte:window bind:scrollY={y}/>
 
-  <style lang="scss">
-  .header {
-    position: fixed;
-    top: 10px;
-    left: 0;
-    width: 100%;
-    &__logo-share {
-      padding: 20px;
-      display: inline-flex;
-      align-items: center;
-      & :global(.share-button) {
-        margin: 0 0 0 10px;
-      }
+<script>
+  import MirrorMediaLogo from './MirrorMediaLogo.svelte'
+  import ShareButton from './ShareButton.svelte'
+  import { fade } from 'svelte/transition'
+
+  let y = 0
+  let oldY = 0
+
+  let showHeader = true
+
+  $: if (oldY !== y) {
+    const isScrollingUp = y <= oldY
+    showHeader = isScrollingUp
+    oldY = y
+  }
+</script>
+
+<style lang="scss">
+.header {
+  position: fixed;
+  top: 10px;
+  left: 0;
+  width: 100%;
+  &__logo-share {
+    padding: 20px;
+    display: inline-flex;
+    align-items: center;
+    & :global(.share-button) {
+      margin: 0 0 0 10px;
     }
   }
-  </style>
+}
+</style>
