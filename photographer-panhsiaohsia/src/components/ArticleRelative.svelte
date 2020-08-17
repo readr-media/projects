6a65aff894100b69ccbc71848fc5a257c295254a
kwhsiung <kaiwenhsiung@gmail.com>
2020-08-17 19:01:30 +0800

    feat(photographer-panhsiaohsia): add desktop article relatives styles

--- photographer-panhsiaohsia/src/components/ArticleRelative.svelte
+++ photographer-panhsiaohsia/src/components/ArticleRelative.svelte
@@ -47,4 +47,26 @@ export let description = ''
       margin: 10px 0 0 0;
     }
   }
+
+  @media (min-width: 768px) {
+    .article-relatives {
+      display: block;
+      width: 180px;
+    }
+
+    .img-wrapper {
+      width: 100%;
+      padding-top: 0;
+      height: 120px;
+      position: relative;
+      img {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: white;
+      }
+    }
+  }
 </style>
