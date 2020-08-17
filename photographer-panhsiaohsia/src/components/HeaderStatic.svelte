1e84297dbb99972805d04fd3c9ba51661f856a2a
kwhsiung <kaiwenhsiung@gmail.com>
2020-08-17 16:00:33 +0800

    feat(photographer-panhsiaohsia): add HeaderStatic

---
+++ photographer-panhsiaohsia/src/components/HeaderStatic.svelte
@@ -0,0 +1,32 @@
+<header class="header" bind:this={el}>
+  <div class="header__logo-share">
+    <MirrorMediaLogo class="mirror-media-logo" />
+    <ShareNav class="share-nav" />
+  </div>
+</header>
+
+<script>
+  import MirrorMediaLogo from './MirrorMediaLogo.svelte'
+  import ShareNav from './ShareNav.svelte'
+  export let el
+</script>
+
+<style lang="scss">
+.header {
+  width: 100%;
+  z-index: 10000;
+  background-color: white;
+  position: relative;
+  height: 80px;
+  &__logo-share {
+    position: absolute;
+    top: 0;
+    left: 0;
+    padding: 20px;
+    display: inline-flex;
+    & :global(.share-nav) {
+      margin: 0 0 0 10px;
+    }
+  }
+}
+</style>
